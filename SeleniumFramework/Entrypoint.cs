10ea3f784199ea8b52bb284abdef01ba9b4037aa
Daniyal Waris <daniyal.waris@yahoo.com>
2020-06-08 03:09:27 +0200

    Commit All New Changes

---
+++ SeleniumFramework/Entrypoint.cs
@@ -0,0 +1,14 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+internal class EntryPoint
+{
+    private static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.google.com/");
+        Thread.Sleep(3000);
+        driver.Quit();
+
+    }
+}
